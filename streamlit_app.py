d30c48e905c4ea55de7b22cd4502208163bf0a06
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-21 15:57:01 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,13 @@
 import streamlit as st
-st.write(st.secrets)
+import requests
+# st.write(st.secrets)
+
+code = st.secrets[code]
+client_id = st.secrets[client_id]
+client_secret = st.secrets[client_secret]
+redirect_uri = "https://jacobsummit-sme-hub-streamlit-app-z2fgzo.streamlit.app/"
+
+
+response = requests.get(f"https://accounts.zoho.com/oauth/v2/token?code={code}&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&grant_type=authorization_code")
+
+st.write(response.status_code)
