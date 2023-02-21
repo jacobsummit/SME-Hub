0740330eb37b3d74149d5a449e3e0ba22b86ebbd
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-21 16:22:19 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ code = st.secrets["code"]
 client_id = st.secrets["client_id"]
 client_secret = st.secrets["client_secret"]
 redirect_uri = "https://jacobsummit-sme-hub-streamlit-app-z2fgzo.streamlit.app/"
-refresh_token = st.secrets("refresh-token")
+refresh_token = st.secrets["refresh-token"]
 
 token_refresh = requests.post(f"https://accounts.zoho.com/oauth/v2/token?refresh_token={refresh_token}&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&grant_type=refresh_token")
 # response = requests.post(f"https://accounts.zoho.com/oauth/v2/token?code={code}&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&grant_type=authorization_code")
