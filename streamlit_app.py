8660b2d95424270c0416edffa0c092a7fa1906d8
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-21 16:23:29 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,5 @@ refresh_token = st.secrets["refresh-token"]
 
 token_refresh = requests.post(f"https://accounts.zoho.com/oauth/v2/token?refresh_token={refresh_token}&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&grant_type=refresh_token")
 # response = requests.post(f"https://accounts.zoho.com/oauth/v2/token?code={code}&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&grant_type=authorization_code")
-st.write(token_refresh.json())
+access_token = token_refresh.json()["access_token"]
+st.write(access_token)
