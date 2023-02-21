790c87a2cf30ec664c832e77fe4bbc9fd7c91cf2
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-21 16:01:58 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ redirect_uri = "https://jacobsummit-sme-hub-streamlit-app-z2fgzo.streamlit.app/"
 
 response = requests.get(f"https://accounts.zoho.com/oauth/v2/token?code={code}&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&grant_type=authorization_code")
 
-st.write(response.status_code)
+st.write(response)
