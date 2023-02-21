edf0a7b887ff2788ff6443e118fb7f352f038b11
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-21 16:57:56 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ token_refresh = requests.post(f"https://accounts.zoho.com/oauth/v2/token?refresh
 access_token = token_refresh.json()["access_token"]
 
 # st.write(access_token)
-dataTest = requests.post(url=f"https://analyticsapi.zoho.com/api/spencer@summitventurestudio.com/2388301000001369040/2388301000003333001")
+dataTest = requests.post(f"https://analyticsapi.zoho.com/api/spencer@summitventurestudio.com/2388301000001369040/2388301000003333001")
 st.write(dataTest.json())
