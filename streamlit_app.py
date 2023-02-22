066cb8d787b730cc8a6f2f27fc9b14e6f5b0343c
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-22 15:54:03 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -167,4 +167,4 @@ tmph = tempfile.NamedTemporaryFile(delete=False)
 
 HTML(t.to_html(tmph.name, escape=False, index=False))
 
-st.markdown(tmph.name, unsafe_allow_html=True)
+st.markdown(str(tmph), unsafe_allow_html=True)
