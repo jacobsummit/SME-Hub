dbaa2a59bece63b5e724e3168e335183ae33a96b
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-22 11:11:56 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ access_token = token_refresh.json()["access_token"]
 # st.write(access_token)
 dataTest = requests.get(f"https://analyticsapi.zoho.com/api/jacob@summitventurestudio.com")
 # dataTest = requests.post(f"https://analyticsapi.zoho.com/api/spencer@summitventurestudio.com/'Zoho CRM + Projects Analytics'/'Pipeline Report for Work with Us Page Darla SVS'")
-st.write(dataTest)
+st.write(requests.get('https://github.com').status_code)
