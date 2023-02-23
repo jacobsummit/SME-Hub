e9b0a687973aa4ed7aed3538ce6fd7ba50ce2b3f
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-23 16:56:40 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,8 @@ custom_css = {
     ".ag-header-cell-text":{"color":"#fff","font-size":"15px !important"},
     ".ag-header":{"background":"linear-gradient(90deg, rgba(218,120,34,1) 0%, rgba(163,31,36,1) 100%);"},
     ".ag-cell-wrap-text":{"word-break":"break-word"},
-    ".ag-root-wrapper":{"border-radius":"1em","box-shadow":  "0 0 20px rgba(0, 0, 0, 0.15)"}
+    ".ag-root-wrapper":{"border-radius":"1em"},
+    ".ag-grid-container":{"box-shadow":  "0 0 20px rgba(0, 0, 0, 0.15)"}
 }
 
 
