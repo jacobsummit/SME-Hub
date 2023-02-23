3f6ee88c51ba56ad724312624f9f6502f90b655c
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-23 15:42:25 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,9 +115,10 @@ st.markdown("""
 
 
 gb = GridOptionsBuilder.from_dataframe(df)
-gb.configure_default_column(maxWidth=200)
+# gb.configure_default_column(maxWidth=200)
 gb.configure_columns(["Summary","Project Name","Questions We Need Answered"],wrapText = True,autoHeight = True)
 gb.configure_columns(["1", "2", "3", "4", "5", "6"],width=50, resizable=False)
+gb.configure_columns(["Project ID"],hide=True)
 go = gb.build()
 
 
