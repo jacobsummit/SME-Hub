a1c12bda4c5b08826f13d95defbd2e0a816783a0
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-23 18:12:52 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,14 @@ import json
 import requests
 import tempfile
 import io
+from st_pages import page, show_pages, add_page_title
 from IPython.display import HTML
 from AnalyticsClient import AnalyticsClient
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 
 
 st.set_page_config(layout='wide', page_icon="mountain", page_title="SME Hub")
+add_page_title("Home")
 
 def button_func(row):
     val = f'''<button onclick="window.open('https://smehub.zohocreatorportal.com/#Form:Interest_Form?Project_ID={str(row['Project ID'])}&Project_Name={row['Project Name']}&Project_Owner={row['Project Owner']}&Project_Owner_Email={row["Project Owner Email"]}&AM_Name={row["SVS acct. mgr."]}&AM_Email={row["AM Email"]}')">Help Us <span class="glyphicon glyphicon-new-window"></span></button>'''
