3b3f9e0b78e870a2bc228fbd6eafe677af0b9408
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-24 16:58:12 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,7 +105,7 @@ function(params){
 
 
 gb = GridOptionsBuilder.from_dataframe(df)
-gb.sideBar = {'toolPanels': ["Filters"]}
+gb.sideBar = {'toolPanels': ["filters"]}
 gb.configure_default_column(sizeColumnsToFit=True, enablePivot=False, enableValue=True, enableRowGroup=True, suppressColumnsToolPanel=True)
 gb.configure_selection(selection_mode="multiple", use_checkbox=True)
 gb.configure_column("Project Name", headerTooltip="The name of the technology")
