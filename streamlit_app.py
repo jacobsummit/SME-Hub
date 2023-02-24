9ea432da19da0e7ff6a47a0d9e93f5ebacc3b942
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-24 13:26:10 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,13 +163,14 @@ cellRenderer_addButton = JsCode('''
 
 
 df = df[df['Industry'].isin(industry_choice)]
+st.sidebar.write("test")
 
 gb = GridOptionsBuilder.from_dataframe(df)
 gb.configure_default_column(sizeColumnsToFit=True)
 gb.configure_columns(["Summary","Project Name","Questions We Need Answered"],wrapText = True,autoHeight = True, flex=1)
 gb.configure_columns(["1", "2", "3", "4", "5", "6"],maxWidth=60, resizable=False, cellStyle=cellstyle_jscode,wrapText = True)
 gb.configure_columns(["Project ID","Project Owner", "Project Owner Email", "SVS acct. mgr.", "AM Email"],hide=True)
-gb.configure_column("apple", cellRenderer=cellRenderer_addButton, maxWidth=60)
+gb.configure_column("apple", cellRenderer=cellRenderer_addButton, maxWidth=80)
 
 go = gb.build()
 
