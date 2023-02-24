6ccce2db7baad70d19f539a323179254f1b3f45a
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-24 15:55:32 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,10 +106,10 @@ st.sidebar.write("Projects in Chosen Industry Area(s):",str(len(ind_df)))
 
 gb = GridOptionsBuilder.from_dataframe(df)
 gb.sideBar = {'toolPanels': []}
-gb.configure_default_column(sizeColumnsToFit=True, enablePivot=False, enableValue=True, enableRowGroup=True)
+gb.configure_default_column(sizeColumnsToFit=True, enablePivot=False, enableValue=True, enableRowGroup=True, suppressColumnsToolPanel=True)
 gb.configure_selection(selection_mode="multiple", use_checkbox=True)
 gb.configure_column("Project Name")
-gb.suppressColumnsToolPanel(True)
+# gb.suppressColumnsToolPanel(True)
 # gb.configure_side_bar({'toolPanels': []})
 
 gb.configure_columns(["Summary","Project Name","Questions We Need Answered","Industry"],wrapText = True,autoHeight = True, flex=1)
