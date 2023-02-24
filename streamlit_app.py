c466216360d67f50ba0d7a8a27db69bd18f11ef9
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-24 14:02:46 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -170,7 +170,7 @@ gb.configure_default_column(sizeColumnsToFit=True)
 gb.configure_columns(["Summary","Project Name","Questions We Need Answered","Industry"],wrapText = True,autoHeight = True, flex=1)
 gb.configure_columns(["1", "2", "3", "4", "5", "6"],maxWidth=50, resizable=False, cellStyle=cellstyle_jscode,wrapText = True)
 gb.configure_columns(["Project ID","Project Owner", "Project Owner Email", "SVS acct. mgr.", "AM Email"],hide=True)
-gb.configure_column('Interested?', editable=True, cellRenderer=checkbox_renderer, maxWidth=80)
+gb.configure_column('Interested?', editable=True, cellRenderer=checkbox_renderer, maxWidth=90)
 
 go = gb.build()
 
