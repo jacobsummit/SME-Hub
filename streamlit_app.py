6c8baa7841994f2315cded6348c45c99e4833aaa
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-24 14:58:56 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -144,9 +144,9 @@ gb.configure_default_column(sizeColumnsToFit=True, enablePivot=False, enableValu
 gb.configure_selection(selection_mode="multiple", use_checkbox=True)
 gb.configure_side_bar()
 
-gb.configure_columns(["Summary","Project Name","Questions We Need Answered","Industry"],wrapText = True,autoHeight = True, flex=1)
-gb.configure_columns(["1", "2", "3", "4", "5", "6"],maxWidth=50, resizable=False, cellStyle=cellstyle_jscode,wrapText = True)
-gb.configure_columns(["Project ID","Project Owner", "Project Owner Email", "SVS acct. mgr.", "AM Email"],hide=True)
+# gb.configure_columns(["Summary","Project Name","Questions We Need Answered","Industry"],wrapText = True,autoHeight = True, flex=1)
+# gb.configure_columns(["1", "2", "3", "4", "5", "6"],maxWidth=50, resizable=False, cellStyle=cellstyle_jscode,wrapText = True)
+# gb.configure_columns(["Project ID","Project Owner", "Project Owner Email", "SVS acct. mgr.", "AM Email"],hide=True)
 # gb.configure_column('Interested?', editable=True, cellRenderer=checkbox_renderer, maxWidth=90)
 
 go = gb.build()
