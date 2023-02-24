53241d4eb7e2f8814533f50ffa452e7df5154094
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-23 18:40:31 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,7 +130,7 @@ df = df[df['Industry'].isin(industry_choice)]
 
 gb = GridOptionsBuilder.from_dataframe(df)
 gb.configure_default_column(sizeColumnsToFit=True)
-gb.configure_columns(["Summary","Project Name","Questions We Need Answered"],wrapText = True,autoHeight = True, flex=1)
+gb.configure_columns(["Summary","Project Name","Questions We Need Answered"],wrapText = True, flex=1)
 gb.configure_columns(["1", "2", "3", "4", "5", "6"],width=60, resizable=False, cellStyle=cellstyle_jscode)
 gb.configure_columns(["Project ID","Project Owner", "Project Owner Email", "SVS acct. mgr.", "AM Email"],hide=True)
 go = gb.build()
