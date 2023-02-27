9b2c56335560be9de68e987d5c6c895054ea7c02
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-27 14:25:37 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -147,7 +147,7 @@ def anaEmail(anav, fullname, useremail):
     contents += f"Name of the person espressing interest: {fullname}<br>Email of the person expressing interest: {useremail}<br> Below are the project names and URLs they have expressed interest in:<br><br>"
 
     for tech in range(len(anav)):
-        contents += f"<p>Project Name: {v.iloc[tech, 7]}</p>"
+        contents += f"<p>Project Name: {anav.iloc[tech, 7]}</p>"
         # contents += f"<p>Project url: <a href='https://projects.zoho.com/portal/summitventurestudiodotcom#project/{v.iloc[tech, 11]}'>click here</a></p>"
         # contents += f"<p>Your questions about the tech:<br> {v.iloc[tech, 10]}</p>"
 
