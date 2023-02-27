d472963875dac060fc7ea1cb254ed0e2000588a0
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-27 12:28:54 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,5 @@
 import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.header import Header
-from email.mime.application import MIMEApplication
-
-# from google.auth.transport.requests import Request
-# from google.oauth2.credentials import Credentials
-# from google_auth_oauthlib.flow import InstalledAppFlow
-# from googleapiclient.discovery import build
-# from googleapiclient.errors import HttpError
-
+import yagmail
 import streamlit as st
 import pandas as pd
 import requests
