daac62789b0d9ed1bbe9bab1ea1594ca1bcf3c40
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-28 14:31:28 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,10 +43,10 @@ background-color: gold;
 margin-bottom: -4px;
 }</style><br>&nbsp;
 <div class="green-square"></div>
-<span style="float:right; position: relative; right:0px;">= Complete</span>
+<span style="float:right; position: relative; right:0px;">Complete: </span>
 <br/>
 <div class="green-square yellow-square"></div>
-<span style="float:right;"> = In Process</span>""",unsafe_allow_html=True)
+<span style="float:right;">In Process: </span>""",unsafe_allow_html=True)
 st.sidebar.header("SME HUB")
 st.sidebar.write("""Welcome to Summit Venture Studio's SME Hub! Thank you so much for taking the time to help us out.  Here are some instructions:\n
 Please begin by looking through the table to the left and checking the boxes of the projects you are interested in.  
