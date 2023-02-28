3ef92e0acb3b78da5500140f5ea1677fdecd0b58
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-02-28 14:15:24 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ background-color: gold;
 margin-bottom: -4px;
 }</style>
 <div class="green-square"></div>
-<span style="text-align:right; position: relative; right:0px;">= Complete</span>
+<span style="text-align:right;  right:0px;">= Complete</span>
 <br/>
 <div class="green-square yellow-square"></div>
 <span> = In Process</span>""",unsafe_allow_html=True)
