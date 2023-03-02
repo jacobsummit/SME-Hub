8a72ffe048a8131fd4cf5b9ba5380d70773e7c22
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-03-02 16:56:22 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -226,14 +226,13 @@ if email_submit:
         for am in amList:
             # emailer(userEmail, amEmail(v[v["AM Email"]==am], fullName, userEmail), "Message from SME Hub!")
             st.markdown(amEmail(v[v["AM Email"]==am], fullName, userEmail), unsafe_allow_html=True)
+        st.sidebar.success("Email Successfully Sent!")
     if not fullName: st.sidebar.error("Please Enter your Name")
     if not validEmail(userEmail): st.sidebar.error("Please Enter a Valid Email Address")
     if v.empty: st.sidebar.error("Please check at least one box.")
 
             
 # if disButton: st.sidebar.write("Make sure to enter your name, email, and check at least one box")
-
-st.success("yay!")
             
     
 
