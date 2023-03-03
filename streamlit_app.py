872a10a9fdc9ae067dac33997410892c5dd33eca
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-03-02 17:18:27 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ with col2:
 
 with col3:
     st.markdown("""<style>
-    section[data-testid="stSidebar"] .css-6qob1r {{width: 14rem;}}
+    section[data-testid="stSidebar"] .css-1vq4p4l  {{width: 14rem;}}
 .green-square {
 position: relative;
 display: inline-block;
