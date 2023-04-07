c81907c3ca50fd5ccd8f1e35d9d5e0be62b9c051
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-04-07 11:47:58 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -166,7 +166,8 @@ background-color: gold;
 df = load_data()
 
 for row in df.index:
-    st.write(df.loc[row,"Project Name"])
+    with st.expander(df.loc[row,"Project Name"]):
+        st.write(f"**Project Summary:** {df.loc[row,"Summary"]}")
 
 # gb = GridOptionsBuilder.from_dataframe(df)
 # # gb.configure_side_bar(filters_panel=True, columns_panel=False, defaultToolPanel="filters")
