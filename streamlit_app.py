8eb806bf98530b754b384ac775f45919a902880a
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-04-07 12:09:56 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,6 +165,9 @@ with col1:
 
 df = load_data()
 
+filCol1, filCol2 = st.columns((1,1))
+with filCol1:
+    indFil = st.multiselect("Select Industry", df["Industry"].unique())
 for row in df.index:
     with st.expander(df.loc[row,"Project Name"]):
         st.write(f"**Project Summary:** {df.loc[row,'Summary']}")
