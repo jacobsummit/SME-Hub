04f7aaeb34d19c555344d0c9000363f80c711939
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-04-18 13:03:17 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,7 +143,7 @@ for row in filtDf.index:
     with st.expander(f"**{filtDf.loc[row,'Project Name']}**"):   
         st.write(f"**Project Summary:** {filtDf.loc[row,'Summary']}")
         st.write(filtDf.loc[row,'Project ID'])
-        st.button("Click if Interested", key=filtDf.loc[row,"Project ID"], on_click=proj_changed, args=(filtDf.loc[row,"Project ID"], True,))
+        st.checkbox("Click if Interested", key=filtDf.loc[row,"Project ID"], on_changed=proj_changed, args=(filtDf.loc[row,"Project ID"], True))
         # updateDict(df.loc[row,"Project ID"], st.checkbox("Check if Interested", key=df.loc[row,"Project ID"]))
 
 # with st.expander("See Your interests here"):
