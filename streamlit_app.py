41a7f19db97d79cccd6b9fd2733fe7f034a98e84
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-04-18 13:38:07 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,7 +112,7 @@ def updateDf(rowid, val):
 def proj_changed(rowid, val):
     if rowid not in [x["id"] for x in st.session_state.projs]:
         st.session_state.projs.append(
-            {
+            rowid:{
                 "id":rowid,
                 "interest": True
             }
