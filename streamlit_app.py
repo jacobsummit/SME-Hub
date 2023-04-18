754303edafaf43c5cc957711b536a3af811b4ef9
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-04-18 16:08:12 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,7 +165,7 @@ for row in filtDf.index:
 # st.session_state.projs
 # st.write()
 
-st.markdown("<style>.redlabel{color:orange;}.blacklabel{color:black;}</style>", unsafe_allow_html=True)
+st.markdown("<style>.redlabel{color:orange;}</style>", unsafe_allow_html=True)
 
 
 
@@ -179,18 +179,9 @@ with col1:
             userEmail = st.text_input(label = "Email", placeholder="Enter Email")
             email_submit = st.form_submit_button("Submit")
 jsTest = ""
-for name in df["Project Name"]:
-    if name in st.session_state.projs:
-        cssVal = "redlabel"
-    else: cssVal = "blacklabel"
-    jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.add('{cssVal}');"""
+for name in [df[df["Project ID"] == x]["Project Name"].values[0] for x in df["Project ID"] if x in st.session_state.projs]:
+    jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.add('redlabel');"""
 
-st.write([df[df["Project ID"] == x]["Project Name"].values[0] for x in df["Project ID"] if x in st.session_state.projs])
-for name in df["Project Name"]:
-    st.write(name)
-    if name in st.session_state.projs:
-        st.write("found!!")
-st.write(jsTest)
 components.html(f"""
 <script>
 {jsTest}
