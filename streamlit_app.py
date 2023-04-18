a408125d862a3704385c1b872a0db9c37d8b2c6c
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-04-18 15:06:17 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -181,7 +181,7 @@ with col1:
 
 
 
-st.write([df[df["Project ID"] == x] for x in df["Project ID"][0] if x in st.session_state.projs])
+st.write([df[df["Project ID"] == x]["Project Name"] for x in df["Project ID"] if x in st.session_state.projs])
 
 
 #  custom_css = {
