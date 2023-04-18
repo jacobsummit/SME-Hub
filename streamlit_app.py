85ee5cbe5cd22e085c09ca1c602d517d6a261d2e
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-04-18 16:03:03 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -186,7 +186,8 @@ for name in df["Project Name"]:
     jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.add('{cssVal}');"""
 
 st.write([df[df["Project ID"] == x]["Project Name"].values[0] for x in df["Project ID"] if x in st.session_state.projs])
-st.write(df["Project Name"])
+for name in df["Project Name"]:
+    st.write(name)
 st.write(jsTest)
 components.html(f"""
 <script>
