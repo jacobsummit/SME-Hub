981d52c8dd6315441dec5ba4db2b0e95666d4b98
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-04-19 13:14:31 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,12 +100,6 @@ def emailer(useremail, contents, subject):
 
 
 
-st.markdown("""<style>
-.olabel {
-    color: orange;
-}
-
-</style>""", unsafe_allow_html=True)
 
 
 
@@ -122,13 +116,6 @@ def proj_changed(rowid, val):
         st.session_state.projs.append(rowid)
     else:
         st.session_state.projs.remove(rowid)
-    name = df[df["Project ID"] == rowid]["Project Name"].values[0]
-    components.html(f"""
-    <script>
-    Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.toggle('olabel');
-    </script>
-    """)
-    
 
 if "projs" not in st.session_state:
     st.session_state.projs = []
@@ -188,22 +175,28 @@ with col1:
             userEmail = st.text_input(label = "Email", placeholder="Enter Email")
             email_submit = st.form_submit_button("Submit")
 
+st.markdown("""<style>
+.myclass .olabel {
+    color: orange;
+}
+
+</style>""", unsafe_allow_html=True)
 
 
 jsTest = ""
 
-# for name in filtDf["Project Name"]:
-#     cssColor = 'myclass olabel'
-#     jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.toggle('{cssColor}');"""
+for name in filtDf["Project Name"]:
+    cssColor = 'myclass olabel'
+    jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.toggle('{cssColor}');"""
 # for name in [df[df["Project ID"] == x]["Project Name"].values[0] for x in df["Project ID"] if x in st.session_state.projs]:
-#     jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.toggle('olabel');"""
+#     jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.add('redlabel');"""
 
-# components.html(f"""
-# <script>
-# {jsTest}
-# </script>
-# """)
-# st.write(jsTest)
+components.html(f"""
+<script>
+{jsTest}
+</script>
+""")
+st.write(jsTest)
 
 #  custom_css = {
 #     ".ag-header-cell-text":{"color":"#fff","font-size":"15px !important"},
