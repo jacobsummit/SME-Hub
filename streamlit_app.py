50f173a596b3675dc6091579344acd6d676cfdd5
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-04-19 12:10:57 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -182,7 +182,7 @@ jsTest = ""
 
 for name in df["Project Name"]:
     cssColor = 'redlabel'
-    jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.add({cssColor});"""
+    jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.add('{cssColor}');"""
 # for name in [df[df["Project ID"] == x]["Project Name"].values[0] for x in df["Project ID"] if x in st.session_state.projs]:
 #     jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.add('redlabel');"""
 st.write(jsTest)
