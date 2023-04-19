f3cd83260da2b97e2f5875bb22ffc320b786cb67
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-04-19 12:37:13 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -166,17 +166,14 @@ for row in filtDf.index:
 # st.write()
 
 st.markdown("""<style>
-
-.myclass .blacklabel {
-    color: red;
-}
-.redlabel {
+.myclass .redlabel {
     color: orange;
 }
 
-
+.blacklabel {
+    color: black;
+}
 </style>""", unsafe_allow_html=True)
-
 
 
 
@@ -192,7 +189,7 @@ with col1:
 jsTest = ""
 
 for name in df["Project Name"]:
-    cssColor = '.blacklabel'
+    cssColor = 'redlabel'
     jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.add({cssColor});"""
 # for name in [df[df["Project ID"] == x]["Project Name"].values[0] for x in df["Project ID"] if x in st.session_state.projs]:
 #     jsTest += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.add('redlabel');"""
