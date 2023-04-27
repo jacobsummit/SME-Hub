0a446b67185f7a5c4a5153c41019e388a4707e73
jacobsummit <106693868+jacobsummit@users.noreply.github.com>
2023-04-27 15:46:16 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,7 +120,7 @@ def proj_changed(rowid, val):
         st.session_state.projs.remove(rowid)
     sesNames = [df[df["Project ID"] == x]["Project Name"].values[0] for x in df["Project ID"] if x in st.session_state.projs]
     colJs = ""
-    for name in df["Project Name"].values[0]:
+    for name in df["Project Name"]:
         if name in sesNames:
             colJs += f"""Array.from(window.parent.document.querySelectorAll('div[data-testid="stExpander"] div[role="button"] p')).find(el => el.innerText === '{name}').classList.add('olabel');"""
         else:
